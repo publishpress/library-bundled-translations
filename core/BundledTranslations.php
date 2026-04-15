6a1012c27b8185025164372af6328f7a912811dc
Adedeji Adesina <adesinafelix98@gmail.com>
2026-04-15 17:40:10 +0100

    remove debugging added for testing

--- core/BundledTranslations.php
+++ core/BundledTranslations.php
@@ -29,13 +29,6 @@ class BundledTranslations
     private $pluginFile;
 
     /**
-     * Track redirected mo files for debug notice.
-     *
-     * @var array
-     */
-    private $redirectedFiles = [];
-
-    /**
      * @param string $domain       The plugin text domain.
      * @param string $languagesDir Absolute path to the plugin's bundled languages directory.
      * @param string $pluginFile   Absolute path to the main plugin file.
@@ -61,8 +54,6 @@ class BundledTranslations
         }
 
         add_filter('load_textdomain_mofile', [$this, 'filterMoFile'], 10, 2);
-        add_action('admin_notices', [$this, 'showDebugNotice']);
-        add_action('wp_ajax_publishpress_dismiss_bundled_translations_notice', [$this, 'dismissNotice']);
     }
 
     /**
@@ -110,67 +101,6 @@ class BundledTranslations
             return $mofile;
         }
 
-        $this->redirectedFiles[] = [
-            'from' => $mofile,
-            'to' => $pluginMofile,
-            'locale' => $locale
-        ];
-
         return $pluginMofile;
-    }
-
-    /**
-     * Show admin notice with mo file paths being used (for debugging).
-     *
-     * @return void
-     */
-    public function showDebugNotice()
-    {
-        if (empty($this->redirectedFiles)) {
-            return;
-        }
-
-        $dismissKey = 'publishpress_bundled_translations_notice_' . md5($this->domain);
-        
-        if (get_user_meta(get_current_user_id(), $dismissKey, true)) {
-            return;
-        }
-
-        $file = $this->redirectedFiles[0];
-        ?>
-        <div class="notice notice-info is-dismissible" data-dismiss-key="<?php echo esc_attr($dismissKey); ?>">
-            <p>
-                <strong><?php echo esc_html($this->domain); ?> - Bundled Translations Active</strong><br>
-                Using: <code><?php echo esc_html($file['to']); ?></code><br>
-                <small>Instead of: <code><?php echo esc_html($file['from']); ?></code></small>
-            </p>
-        </div>
-        <script>
-        jQuery(document).ready(function($) {
-            $(document).on('click', '[data-dismiss-key="<?php echo esc_js($dismissKey); ?>"] .notice-dismiss', function() {
-                $.post(ajaxurl, {
-                    action: 'publishpress_dismiss_bundled_translations_notice',
-                    dismiss_key: '<?php echo esc_js($dismissKey); ?>',
-                    nonce: '<?php echo wp_create_nonce('dismiss_bundled_translations'); ?>'
-                });
-            });
-        });
-        </script>
-        <?php
-    }
-
-    /**
-     * Handle AJAX request to dismiss the notice.
-     *
-     * @return void
-     */
-    public function dismissNotice()
-    {
-        check_ajax_referer('dismiss_bundled_translations', 'nonce');
-        
-        $dismissKey = sanitize_text_field($_POST['dismiss_key']);
-        update_user_meta(get_current_user_id(), $dismissKey, true);
-        
-        wp_send_json_success();
     }
 }
